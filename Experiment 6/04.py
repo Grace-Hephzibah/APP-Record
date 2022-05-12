6393426dd9ab1b120bafaada55be78859d10f371
Grace-Hephzibah <mgracehephzibah@gmail.com>
2022-05-12 16:16:23 +0530

    Changes

--- Experiment 6/04.py
+++ Experiment 6/04.py
@@ -2,7 +2,6 @@ for num in range(10,20):
     check = 0
     for i in range(2,num):    
         if num%i == 0:         
-            j=num/i
             check = 1
             break
     if check == 0:
